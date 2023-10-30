fa6465a98efe160f48308814b3303f2c70ec9732
GabrielaVidal7 <ga28bvidal@gmail.com>
2023-10-30 10:33:10 -0300

    Novo treinamento

--- 1st_training_files/config.py
+++ 1st_training_files/config.py
@@ -1,21 +1,21 @@
 import torch
+
 BATCH_SIZE = 32 # increase / decrease according to GPU memeory
 RESIZE_TO = 512 # resize the image for training and transforms
 NUM_EPOCHS = 100 # number of epochs to train for
-NUM_WORKERS = 4
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-  
-# Images and labels direcotry should be relative to train.py
-TRAIN_DIR_IMAGES = 'dataset/train'
-TRAIN_DIR_LABELS = 'dataset/train'
-VALID_DIR_IMAGES = 'dataset/test'
-VALID_DIR_LABELS = 'dataset/test'
+# training images and XML files directory
+TRAIN_DIR = 'dataset/train'
+# validation images and XML files directory
+VALID_DIR = 'dataset/test'
 # classes: 0 index is reserved for background
 CLASSES = [
     'car', 'person', 'motorcycle', 'truck', 'animal'
 ]
-NUM_CLASSES = len(CLASSES)
-# whether to visualize images after creating the data loaders
+NUM_CLASSES = 5
+# whether to visualize images after crearing the data loaders
 VISUALIZE_TRANSFORMED_IMAGES = False
 # location to save model and plots
 OUT_DIR = '../outputs'
+SAVE_PLOTS_EPOCH = 2 # save loss plots after these many epochs
+SAVE_MODEL_EPOCH = 2 # save model after these many epochs
