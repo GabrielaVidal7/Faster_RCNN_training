931aff616122ff4730d5fa5960696992ca2f5eb7
GabrielaVidal7 <ga28bvidal@gmail.com>
2023-10-30 11:01:56 -0300

    Novo treinamento

--- dataset.py
+++ dataset.py
@@ -3,29 +3,62 @@ import cv2
 import numpy as np
 import os
 import glob as glob
+
 from xml.etree import ElementTree as et
-from config import CLASSES, RESIZE_TO, TRAIN_DIR, VALID_DIR, BATCH_SIZE
+from config import (
+    CLASSES, RESIZE_TO, 
+    TRAIN_DIR_IMAGES, VALID_DIR_IMAGES, 
+    TRAIN_DIR_LABELS, VALID_DIR_LABELS,
+    BATCH_SIZE
+)
 from torch.utils.data import Dataset, DataLoader
-from utils import collate_fn, get_train_transform, get_valid_transform
+from custom_utils import collate_fn, get_train_transform, get_valid_transform
 
 # the dataset class
-class campoDataset(Dataset):
-    def __init__(self, dir_path, width, height, classes, transforms=None):
+class CustomDataset(Dataset):
+    def __init__(
+        self, images_path, labels_path, 
+        width, height, classes, transforms=None
+    ):
         self.transforms = transforms
-        self.dir_path = dir_path
+        self.images_path = images_path
+        self.labels_path = labels_path
         self.height = height
         self.width = width
         self.classes = classes
+        self.image_file_types = ['*.jpg', '*.jpeg', '*.png', '*.ppm']
+        self.all_image_paths = []
         
         # get all the image paths in sorted order
-        self.image_paths = glob.glob(f"{self.dir_path}/*.jpg")
-        self.all_images = [image_path.split('/')[-1] for image_path in self.image_paths]
+        for file_type in self.image_file_types:
+            self.all_image_paths.extend(glob.glob(f"{self.images_path}/{file_type}"))
+        self.all_annot_paths = glob.glob(f"{self.labels_path}/*.xml")
+        # Remove all annotations and images when no object is present.
+        self.read_and_clean()
+        self.all_images = [image_path.split(os.path.sep)[-1] for image_path in self.all_image_paths]
         self.all_images = sorted(self.all_images)
+        
+    def read_and_clean(self):
+        """
+        This function will discard any images and labels when the XML 
+        file does not contain any object.
+        """
+        for annot_path in self.all_annot_paths:
+            tree = et.parse(annot_path)
+            root = tree.getroot()
+            object_present = False
+            for member in root.findall('object'):
+                object_present = True
+            if object_present == False:
+                print(f"Removing {annot_path} and corresponding image")
+                self.all_annot_paths.remove(annot_path)
+                self.all_image_paths.remove(annot_path.split('.xml')[0]+'.jpg')
 
     def __getitem__(self, idx):
         # capture the image name and the full image path
         image_name = self.all_images[idx]
-        image_path = os.path.join(self.dir_path, image_name)
+        image_path = os.path.join(self.images_path, image_name)
+
         # read the image
         image = cv2.imread(image_path)
         # convert BGR to RGB color format
@@ -35,7 +68,7 @@ class campoDataset(Dataset):
         
         # capture the corresponding XML file for getting the annotations
         annot_filename = image_name[:-4] + '.xml'
-        annot_file_path = os.path.join(self.dir_path, annot_filename)
+        annot_file_path = os.path.join(self.labels_path, annot_filename)
         
         boxes = []
         labels = []
@@ -66,9 +99,9 @@ class campoDataset(Dataset):
             xmin_final = (xmin/image_width)*self.width
             xmax_final = (xmax/image_width)*self.width
             ymin_final = (ymin/image_height)*self.height
-            yamx_final = (ymax/image_height)*self.height
+            ymax_final = (ymax/image_height)*self.height
             
-            boxes.append([xmin_final, ymin_final, xmax_final, yamx_final])
+            boxes.append([xmin_final, ymin_final, xmax_final, ymax_final])
         
         # bounding box to tensor
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
@@ -78,6 +111,7 @@ class campoDataset(Dataset):
         iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
         # labels to tensor
         labels = torch.as_tensor(labels, dtype=torch.int64)
+
         # prepare the final `target` dictionary
         target = {}
         target["boxes"] = boxes
@@ -88,60 +122,75 @@ class campoDataset(Dataset):
         target["image_id"] = image_id
         # apply the image transforms
         if self.transforms:
-            sample = self.transforms(image = image_resized,
-                                     bboxes = target['boxes'],
-                                     labels = labels)
+            sample = self.transforms(image=image_resized,
+                                     bboxes=target['boxes'],
+                                     labels=labels)
             image_resized = sample['image']
             target['boxes'] = torch.Tensor(sample['bboxes'])
             
         return image_resized, target
-    
+
     def __len__(self):
         return len(self.all_images)
-    
+
 # prepare the final datasets and data loaders
-train_dataset = campoDataset(TRAIN_DIR, RESIZE_TO, RESIZE_TO, CLASSES, get_train_transform())
-valid_dataset = campoDataset(VALID_DIR, RESIZE_TO, RESIZE_TO, CLASSES, get_valid_transform())
-train_loader = DataLoader(
-    train_dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-    num_workers=0,
-    collate_fn=collate_fn
-)
-valid_loader = DataLoader(
-    valid_dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=False,
-    num_workers=0,
-    collate_fn=collate_fn
-)
-print(f"Number of training samples: {len(train_dataset)}")
-print(f"Number of validation samples: {len(valid_dataset)}\n")
+def create_train_dataset():
+    train_dataset = CustomDataset(
+        TRAIN_DIR_IMAGES, TRAIN_DIR_LABELS,
+        RESIZE_TO, RESIZE_TO, CLASSES, get_train_transform()
+    )
+    return train_dataset
+def create_valid_dataset():
+    valid_dataset = CustomDataset(
+        VALID_DIR_IMAGES, VALID_DIR_LABELS, 
+        RESIZE_TO, RESIZE_TO, CLASSES, get_valid_transform()
+    )
+    return valid_dataset
+
+def create_train_loader(train_dataset, num_workers=0):
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        num_workers=num_workers,
+        collate_fn=collate_fn
+    )
+    return train_loader
+def create_valid_loader(valid_dataset, num_workers=0):
+    valid_loader = DataLoader(
+        valid_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=num_workers,
+        collate_fn=collate_fn
+    )
+    return valid_loader
+
 
 # execute datasets.py using Python command from Terminal...
 # ... to visualize sample images
 # USAGE: python datasets.py
 if __name__ == '__main__':
     # sanity check of the Dataset pipeline with sample visualization
-    dataset = campoDataset(
-        TRAIN_DIR, RESIZE_TO, RESIZE_TO, CLASSES
+    dataset = CustomDataset(
+        TRAIN_DIR_IMAGES, RESIZE_TO, RESIZE_TO, CLASSES
     )
     print(f"Number of training images: {len(dataset)}")
     
     # function to visualize a single sample
     def visualize_sample(image, target):
-        box = target['boxes'][0]
-        label = CLASSES[target['labels']]
-        cv2.rectangle(
-            image, 
-            (int(box[0]), int(box[1])), (int(box[2]), int(box[3])),
-            (0, 255, 0), 1
-        )
-        cv2.putText(
-            image, label, (int(box[0]), int(box[1]-5)), 
-            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2
-        )
+        for box_num in range(len(target['boxes'])):
+            box = target['boxes'][box_num]
+            label = CLASSES[target['labels'][box_num]]
+            cv2.rectangle(
+                image, 
+                (int(box[0]), int(box[1])), (int(box[2]), int(box[3])),
+                (0, 255, 0), 2
+            )
+            cv2.putText(
+                image, label, (int(box[0]), int(box[1]-5)), 
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2
+            )
         cv2.imshow('Image', image)
         cv2.waitKey(0)
         
