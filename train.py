33521fe5a27e90cb816f606ee21ff5a120112ffe
GabrielaVidal7 <ga28bvidal@gmail.com>
2023-10-30 14:09:14 -0300

    Novo treinamento

--- train.py
+++ train.py
@@ -16,6 +16,7 @@ from custom_utils import (
     save_train_loss_plot,
     Averager, show_tranformed_image
 )
+
 import torch
 
 if __name__ == '__main__':
@@ -25,13 +26,16 @@ if __name__ == '__main__':
     valid_loader = create_valid_loader(valid_dataset, NUM_WORKERS)
     print(f"Number of training samples: {len(train_dataset)}")
     print(f"Number of validation samples: {len(valid_dataset)}\n")
+
     if VISUALIZE_TRANSFORMED_IMAGES:
         show_tranformed_image(train_loader)
+
     # Initialize the Averager class.
     train_loss_hist = Averager()
     # Train and validation loss lists to store loss values of all
     # iterations till ena and plot graphs for all iterations.
     train_loss_list = []
+
     # Initialize the model and move to the computation device.
     model = create_model(num_classes=NUM_CLASSES)
     model = model.to(DEVICE)
@@ -46,6 +50,7 @@ if __name__ == '__main__':
     # Define the optimizer.
     # optimizer = torch.optim.SGD(params, lr=0.001, momentum=0.9, weight_decay=0.0005)
     optimizer = torch.optim.AdamW(params, lr=0.0001, weight_decay=0.0005)
+
     # LR will be zero as we approach `steps` number of epochs each time.
     # If `steps = 5`, LR will slowly reduce to zero every 5 epochs.
     steps = NUM_EPOCHS + 25
@@ -55,8 +60,10 @@ if __name__ == '__main__':
         T_mult=1,
         verbose=True
     )
+
     for epoch in range(NUM_EPOCHS):
         train_loss_hist.reset()
+
         _, batch_loss_list = train_one_epoch(
             model, 
             optimizer, 
@@ -67,10 +74,14 @@ if __name__ == '__main__':
             print_freq=100,
             scheduler=scheduler
         )
+
         evaluate(model, valid_loader, device=DEVICE)
+
         # Add the current epoch's batch-wise lossed to the `train_loss_list`.
         train_loss_list.extend(batch_loss_list)
+
         # Save the current epoch model.
         save_model(OUT_DIR, epoch, model, optimizer)
+
         # Save loss plot.
         save_train_loss_plot(OUT_DIR, train_loss_list)
